0153be8f4fc5db4717f333e52321cfe87ef7fe74
ZacharyGoolman <zachary.goolman@gmail.com>
2022-03-30 15:46:08 -0400

    Created Car Serializer Class Info

---
+++ cars/serializers.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import Car
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car()
+        fields = ['id', 'make', 'model', 'year', 'price']
