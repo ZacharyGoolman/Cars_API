c17082696a60dc9d936b8fb1e194ffb3e5b18204
ZacharyGoolman <zachary.goolman@gmail.com>
2022-03-30 15:35:45 -0400

    Created Car Model For Cars App + Registered Cars and Rest Framework in Project Settings Installed Apps

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,13 @@
+from pyexpat import model
 from django.db import models
+from django.forms import IntegerField
 
 # Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    
+    
